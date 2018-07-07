1565f980248006560a1d95ce0bdbf26958ddcb57
Seth Marini <sethmarini@gmail.com>
2018-07-07 12:07:01 -0400

    forgot password css

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase';
+import logo from '../Images/Lead-Ninja.svg'; 
 
 const PasswordForgetPage = () =>
-  <div>
-    <h1>PasswordForget</h1>
-    <PasswordForgetForm />
+<div className="container-fluid h-100">
+<div className="row h-100 colBlue">
+  <div className="col-sm-6 my-auto text-center text-light colBlue">
+    <img src={logo} alt=""/>
+    <h4>We give the finance sector a simple solution to capture and analyze leads.</h4>
   </div>
+  <div className="col-sm-6 colWhite h-100">
+    <div className="row h-100">
+      <div className="col my-auto">
+        <div className="jumbotron m-5">
+          <h3 className="display-4">Password Reset</h3>
+          <PasswordForgetForm />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
 
 const updateByPropertyName = (propertyName, value) => () => ({
   [propertyName]: value,
@@ -47,18 +62,22 @@ class PasswordForgetForm extends Component {
     const isInvalid = email === '';
 
     return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          value={this.state.email}
-          onChange={event => this.setState(updateByPropertyName('email', event.target.value))}
-          type="text"
-          placeholder="Email Address"
-        />
-        <button disabled={isInvalid} type="submit">
-          Reset My Password
-        </button>
 
-        { error && <p>{error.message}</p> }
+      <form className="form-group" onSubmit={this.onSubmit}>
+        <div className="form-group">
+          <input
+            className="form-control"
+            value={this.state.email}
+            onChange={event => this.setState(updateByPropertyName('email', event.target.value))}
+            type="text"
+            placeholder="Email Address"
+        />
+        </div>
+          <button className="btn btn-primary" disabled={isInvalid} type="submit">
+            Reset My Password
+          </button>
+
+          { error && <p>{error.message}</p> }
       </form>
     );
   }
